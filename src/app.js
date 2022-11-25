55dfafebdaa5a59d95ffd39eb45f0cae24b37543
Carlos Alberto <95.carlos.velazquez@gmail.com>
2022-11-25 20:08:15 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,53 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  render();
+};
+
+const generatorButton = document.getElementById("generatorButton");
+
+generatorButton.addEventListener("click", () => {
+  render();
+});
+
+const render = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardNumber = document.getElementById("cardNumber");
+  let cardSuit = document.getElementById("cardSuit");
+  let cardSuitRotated = document.getElementById("cardSuitRotated");
+
+  let cardNumberMap = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  let cardSuitMap = ["♣", "♦", "♥", "♠"];
+
+  cardNumber.innerHTML =
+    cardNumberMap[Math.floor(Math.random() * cardNumberMap.length)];
+
+  cardSuit.innerHTML =
+    cardSuitMap[Math.floor(Math.random() * cardSuitMap.length)];
+  cardSuitRotated.innerHTML = cardSuit.innerHTML;
+
+  if (cardSuit.innerHTML == "♦" || cardSuit.innerHTML == "♥") {
+    cardSuit.style.color = "red";
+    cardSuitRotated.style.color = "red";
+    cardNumber.style.color = "red";
+  } else {
+    cardSuit.style.color = "black";
+    cardSuitRotated.style.color = "black";
+    cardNumber.style.color = "black";
+  }
 };
